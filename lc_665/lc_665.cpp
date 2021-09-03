09f3273c3b64e54e10c06b8f9163274abd6282f3
heoll0 <bupt_liujiang@163.com>
2021-09-03 10:36:18 +0800

    add lc_665

---
+++ lc_665/lc_665.cpp
@@ -0,0 +1,29 @@
+#include<vector>
+using namespace std;
+
+
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int num = 0;
+        for(int i = 0; i < nums.size() - 1; ++i){
+            if(nums[i] > nums[i+1]){
+                ++num;
+                if(i != 0 && nums[i-1] > nums[i+1])
+                    ++num;
+            }
+
+            if(num > 1)
+                return false;            
+        }
+        return true;
+    }
+};
+
+int main(){
+    Solution test;
+    vector<int> nums = {5,7,1,8};
+    test.checkPossibility(nums);
+
+    return 0;
+}
