16f03d6f97303d9018bb2bc48babe87468cc154e
heoll0 <bupt_liujiang@163.com>
2021-09-08 17:18:38 +0800

    modify lc_680

--- lc_680/lc_680.cpp
+++ lc_680/lc_680.cpp
@@ -3,45 +3,32 @@ using namespace std;
 
 class Solution {
 public:
-    bool validPalindrome(string s) {
-        int l = 0, r = s.size()-1;
-        int num_err = 0, loop_num = 0;
-        int l_index = 0, r_index = 0;
+
+    bool check(string s, int l, int r){
         while(r > l){
             if(s[l] == s[r]){
                 ++l;
                 --r;
             }
+            else
+                return false;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int l = 0, r = s.size()-1;
+        while (r > l)
+        {
+            if(s[r] == s[l]){
+                ++l;
+                --r;
+            }
             else{
-                if(loop_num > 0)
-                    return false;
-                if(s[l+1] == s[r]){
-                    ++l;
-                    ++num_err;
-                    if(num_err == 1){
-                        l_index = l-1;
-                        r_index = r;
-                    }
-                    else{
-                        l = l_index;
-                        r = r_index;
-                        num_err = 0;
-                        ++loop_num;
-                    }
-                }
-                else if (s[l] == s[r-1])
-                {
-                    --r;
-                    ++num_err;
-                    ++loop_num;
-                }
-                else
-                {
-                    return false;    
-                }           
+                return check(s, l+1, r) || check(s, l, r-1);
             }
         }
-        return num_err > 1 ? false : true;
+        return true;
     }
 };
 
