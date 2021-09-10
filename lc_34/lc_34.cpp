07572911fca3bab435fdd3ee985358a3219c1006
heoll0 <bupt_liujiang@163.com>
2021-09-10 14:15:05 +0800

    add lc_34

--- lc_34/lc_34.cpp
+++ lc_34/lc_34.cpp
@@ -4,22 +4,40 @@ using namespace std;
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int low = 0, high = nums.size()-1;
-        
-        while(1){
-            int middle = (low + high)/2;
+        int low = 0, high = nums.size()-1, middle;
+        vector<int> result;
+        while(high >= low){
+            middle = (low + high)/2;
             if(nums[middle] > target) high = middle-1;
             else if (nums[middle] < target) low = middle+1;
-            else break;            
+            else{
+                result.push_back(lowwer_bound(nums, target, low, middle));
+                result.push_back(higher_bound(nums, target, middle, high));
+                return result; 
+            }            
         }
+        result.push_back(-1);
+        result.push_back(-1);
+        return result;
     }
 
     int lowwer_bound(vector<int>& nums, int target, int low, int high){
         int low_index = low, high_index = high;
-        while(1){
+        while(high_index >= low_index){
             int middle = (low_index + high_index)/2;
             if(nums[middle] < target) low_index = middle+1;
             if(nums[middle] == target) high_index = middle-1;
         }
+        return low_index;
+    }
+
+    int higher_bound(vector<int>& nums, int target, int low, int high){
+        int low_index = low, high_index = high;
+        while(high_index >= low_index){
+            int middle = (low_index + high_index)/2;
+            if(nums[middle] > target) high_index = middle-1;
+            if(nums[middle] == target) low_index = middle+1;
+        }
+        return high_index;
     }
 };
