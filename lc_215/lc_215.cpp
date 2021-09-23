3be4c87aba069d470d0a0e3adc6c407682ee4d79
heoll0 <bupt_liujiang@163.com>
2021-09-23 19:59:17 +0800

    add lc_215

---
+++ lc_215/lc_215.cpp
@@ -0,0 +1,15 @@
+#include<vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+
+    void quick_sort(vector<int>& nums, int l, int r, int size){
+        
+    }
+
+    int findKthLargest(vector<int>& nums, int k) {
+
+    }
+};
