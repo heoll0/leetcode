db5a9c5825ad25efdf98f8366de6b54aefc006b6
heoll0 <bupt_liujiang@163.com>
2022-03-03 13:09:56 +0800

    modify lc_547

--- lc_547/lc_547.cpp
+++ lc_547/lc_547.cpp
@@ -1,29 +1,36 @@
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int result = isConnected.size();
+        int result = 0;
         for(int i = 0; i < isConnected.size(); ++i){
-            for(int j = i + 1; j < isConnected.size(); ++j){
-                if(isConnected[i][j] == 1 && i != j){
-                    isConnected[i][j] = 0;
-                    isConnected[j][i] = 0;
-                    result -= getConnectedNum(isConnected, j, i);
+            if(isConnected[i][i] == 1){
+                ++result;
+                for(int j = 0; j < isConnected.size(); ++j){
+                    if(isConnected[i][j] == 1 && i != j){
+                        getConnectedNum(isConnected, j);
+                    }
                 }
             }
         }
         return result;
     }
-
-    int getConnectedNum(vector<vector<int>>& isConnected, int j, int i){
-        int Num = 1;
-        for(int x = j + 1; x < isConnected.size(); ++x){
+    void getConnectedNum(vector<vector<int>>& isConnected, int j){
+        isConnected[j][j] = 0;
+        for(int x = 0; x < isConnected.size(); ++x){
             if(isConnected[j][x] == 1){
-                isConnected[j][x] = 0;
-                isConnected[i][x] = 0;
-                Num += getConnectedNum(isConnected, x, j);
+                getConnectedNum(isConnected, x);    
             }
         }
-        return Num;
     }
 
 };
+
+int main(){
+    Solution test;
+    vector<vector<int>> v = {{1,1,0}, {1,1,0}, {0,0,1}};
+    test.findCircleNum(v);
+}
