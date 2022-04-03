4d49dad37ad221cd6c864c684a7624f9fa28a138
heoll0 <bupt_liujiang@163.com>
2022-04-03 13:52:39 +0800

    modify lc_47

--- lc_47/lc_47.cpp
+++ lc_47/lc_47.cpp
@@ -5,19 +5,21 @@ class Solution {
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
         backtrace(nums, 0, nums.size()-1, ans);
         return ans;
     }
-    void backtrace(vector<int> nums, int left, int right, vector<vector<int>> ans){
+    void backtrace(vector<int> nums, int left, int right, vector<vector<int>>& ans){
         if(left == right){
             ans.push_back(nums);
+            return;
         }
         for(int i = left; i <= right; ++i){
             if(nums[i] == nums[left] && i != left){
                 continue;
             }
             swap(nums[i], nums[left]);
-            backtrace(nums, i+1, right, ans);
+            backtrace(nums, left+1, right, ans);
         }
     }
 };
