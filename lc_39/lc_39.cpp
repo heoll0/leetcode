7255470b4a074035e730712b7c79fad0389176f7
heoll0 <bupt_liujiang@163.com>
2022-04-03 15:43:45 +0800

    modify lc_39

--- lc_39/lc_39.cpp
+++ lc_39/lc_39.cpp
@@ -6,11 +6,11 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> record;
-        dfs(candidates, ans, target, record, 0);
+        dfs(candidates, ans, target, record, 0, 0);
         return ans;
     }
 
-    void dfs(vector<int>& candidates, vector<vector<int>>& ans, int target, vector<int>& record, int sum){
+    void dfs(vector<int>& candidates, vector<vector<int>>& ans, int target, vector<int>& record, int sum, int idx){
         if(sum > target){
             return;
         }
@@ -19,10 +19,10 @@ public:
             return;
         }
         else{
-            for(int i = 0; i < candidates.size(); ++i){
+            for(int i = idx; i < candidates.size(); ++i){
                 record.push_back(candidates[i]);
                 sum += candidates[i];
-                dfs(candidates, ans, target, record, sum);
+                dfs(candidates, ans, target, record, sum, i);
                 record.pop_back();
                 sum -= candidates[i];
             }
