07f5bdc8276c6329ce5d1bfeab93249776b3d85a
heoll0 <bupt_liujiang@163.com>
2022-04-10 18:32:13 +0800

    add lc_198

---
+++ lc_198/lc_198.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1){
+            return nums[0];
+        }
+        else if(n == 2){
+            return max(nums[0], nums[1]);
+        }
+        else{
+            int m = max(nums[0], nums[1]);
+            vector<int> dp{nums[0],m};
+            dp.reserve(n);
+            for(int i = 2; i < n; ++i){
+                dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
+            }
+            return dp[n-1];;
+        }
+
+    }
+};
