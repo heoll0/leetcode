37bc6bdb520509c5c1b19df4a0a9e581dda44bfa
heoll0 <bupt_liujiang@163.com>
2022-04-10 18:48:48 +0800

    add another solution

--- lc_198/lc_198.cpp
+++ lc_198/lc_198.cpp
@@ -1,3 +1,4 @@
+// space complexity O(n)
 class Solution {
 public:
     int rob(vector<int>& nums) {
@@ -8,5 +9,23 @@ public:
             dp[i] = max(dp[i-1], dp[i-2] + nums[i-1]);
         }
         return dp[n];
+    }
+};
+
+// space complexity O(1)
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1){
+            return nums[0];
+        }
+        int pre2 = 0, pre1 = nums[0], cur;
+        for(int i = 2; i <= n; ++i){
+            cur = max(pre2 + nums[i-1], pre1);
+            pre2 = pre1;
+            pre1 = cur;
+        }
+        return cur;
     }
 };
