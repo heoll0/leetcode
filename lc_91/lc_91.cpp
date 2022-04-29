351c21a88c20f37806528d25a57362187d3e8925
heoll0 <bupt_liujiang@163.com>
2022-04-29 12:21:16 +0800

    add lc_91

---
+++ lc_91/lc_91.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> dp(n + 1, 1);
+        int pre = s[0] - '0';
+        if(pre == 0){
+            return 0;
+        } 
+        for(int i = 2; i <= n; ++i){
+            
+            int cur = s[i - 1] - '0';
+            if((pre == 0 || pre > 2) && cur == 0){
+                return 0;
+            }
+            if(pre == 1 || (pre == 2 && cur < 7)){
+                if(cur == 0){
+                    dp[i] = dp[i - 2];
+                }
+                else{
+                    dp[i] = dp[i - 1] + dp[i - 2];
+                }
+            }
+            else{
+                dp[i] = dp[i - 1];
+            }
+            pre = cur;
+        }
+        return dp[n];
+    }
+};
