380f0f20450c8dd84b098f11eaa09678178d646e
heoll0 <bupt_liujiang@163.com>
2022-04-29 21:25:21 +0800

    lc_300 add another solution

--- lc_300/lc_300.cpp
+++ lc_300/lc_300.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 class Solution {
 public:
+    // dp O(n^2)
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n, 1);
@@ -14,5 +15,22 @@ public:
             }
         }
         return *max_element(dp.begin(), dp.end());
+    }
+
+    // binary O(nlogn)
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        ans.push_back(nums[0]);
+        for(int i = 1; i < n; ++i){
+            if(nums[i] > ans.back()){
+                ans.push_back(nums[i]);
+            }
+            else{
+                auto iter = lower_bound(ans.begin(), ans.end(), nums[i]);
+                *iter = nums[i];
+            }
+        }
+        return ans.size();
     }
 };
