bbbecffeea471310700918d094a8e1fd231bb710
heoll0 <bupt_liujiang@163.com>
2022-05-03 05:10:50 +0800

    modify lc_91

--- lc_91/lc_91.cpp
+++ lc_91/lc_91.cpp
@@ -5,29 +5,17 @@ class Solution {
 public:
     int numDecodings(string s) {
         int n = s.size();
-        vector<int> dp(n + 1, 1);
-        int pre = s[0] - '0';
-        if(pre == 0){
-            return 0;
-        } 
-        for(int i = 2; i <= n; ++i){
-            
-            int cur = s[i - 1] - '0';
-            if((pre == 0 || pre > 2) && cur == 0){
-                return 0;
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        for(int i = 1; i <= n; ++i){
+            if(s[i-1] != '0'){
+                dp[i] = dp[i-1];
             }
-            if(pre == 1 || (pre == 2 && cur < 7)){
-                if(cur == 0){
-                    dp[i] = dp[i - 2];
-                }
-                else{
-                    dp[i] = dp[i - 1] + dp[i - 2];
+            if(i > 1){
+                if(s[i-2] == '1' || (s[i-2] == '2' && s[i-1] - '0' < 7)){
+                    dp[i] += dp[i-2];
                 }
             }
-            else{
-                dp[i] = dp[i - 1];
-            }
-            pre = cur;
         }
         return dp[n];
     }
